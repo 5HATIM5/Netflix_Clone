d998ae55c048ace04cf1651bcea05d428e666565
Hatim Shabbir <75577655+5HATIM5@users.noreply.github.com>
2021-04-23 01:46:12 +0500

    Nav logo updated

--- src/Nav.js
+++ src/Nav.js
@@ -22,7 +22,7 @@ function Nav() {
         <div className={`nav ${show && "nav_black"}`}>
             <img
                 className="nav_logo"
-                src="https://www.abacustechnologies.com/wp-content/uploads/2020/05/580b57fcd9996e24bc43c529.png"
+                src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
                 alt="netflix_logo"
             />
             <img
